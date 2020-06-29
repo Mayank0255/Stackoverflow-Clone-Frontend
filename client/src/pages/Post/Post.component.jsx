989eca3acddd07530731a0317b7a04bdcee3b5dd
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-06-29 19:18:33 +0530

    fix: remove redundant code

--- client/src/pages/Post/Post.component.jsx
+++ client/src/pages/Post/Post.component.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { getPost,deletePost } from '../../redux/posts/posts.actions';
 import { getAnswers,deleteAnswer, addAnswer } from '../../redux/answers/answers.actions';
 import { getComments,deleteComment,addComment } from '../../redux/comments/comments.actions';
+import timeAgo from "../../utils/timeAgo.utils";
 
 import { ReactComponent as UpVote } from '../../assets/sort-up-solid.svg';
 import { ReactComponent as DownVote } from '../../assets/sort-down-solid.svg';
@@ -52,59 +53,6 @@ const Post = ({ deletePost,deleteAnswer,addAnswer,deleteComment,addComment,getAn
             text: ''
         });
     };
-
-
-    const MONTH_NAMES = [
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
-    ];
-    function getFormattedDate(date, prefomattedDate = false, hideYear = false) {
-        const day = date.getDate();
-        const month = MONTH_NAMES[date.getMonth()];
-        const year = date.getFullYear();
-        const hours = date.getHours();
-        let minutes = date.getMinutes();
-        if (minutes < 10) {
-            minutes = `0${ minutes }`;
-        }
-        if (prefomattedDate) {
-            return `${ prefomattedDate } at ${ hours }:${ minutes }`;
-        }
-        if (hideYear) {
-            return `${ day }. ${ month } at ${ hours }:${ minutes }`;
-        }
-        return `${ day }. ${ month } ${ year }. at ${ hours }:${ minutes }`;
-    }
-    function timeAgo(dateParam) {
-        if (!dateParam) {
-            return null;
-        }
-        const date = typeof dateParam === 'object' ? dateParam : new Date(dateParam);
-        const DAY_IN_MS = 86400000; // 24 * 60 * 60 * 1000
-        const today = new Date();
-        const yesterday = new Date(today - DAY_IN_MS);
-        const seconds = Math.round((today - date) / 1000);
-        const minutes = Math.round(seconds / 60);
-        const isToday = today.toDateString() === date.toDateString();
-        const isYesterday = yesterday.toDateString() === date.toDateString();
-        const isThisYear = today.getFullYear() === date.getFullYear();
-        if (seconds < 5) {
-            return 'now';
-        } else if (seconds < 60) {
-            return `${ seconds } seconds ago`;
-        } else if (seconds < 90) {
-            return 'about a minute ago';
-        } else if (minutes < 60) {
-            return `${ minutes } minutes ago`;
-        } else if (isToday) {
-            return getFormattedDate(date, 'Today'); // Today at 10:20
-        } else if (isYesterday) {
-            return getFormattedDate(date, 'Yesterday'); // Yesterday at 10:20
-        } else if (isThisYear) {
-            return getFormattedDate(date, false, true); // 10. January at 10:20
-        }
-        return getFormattedDate(date); // 10. January 2017. at 10:20
-    }
 
     return loading || post === null ? <Fragment>Loading...</Fragment> : <Fragment>
         <div className='page'>
--- client/src/pages/UserPage/UserPage.component.jsx
+++ client/src/pages/UserPage/UserPage.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getUser } from '../../redux/users/users.actions';
 import { Link } from 'react-router-dom';
+import timeAgo from "../../utils/timeAgo.utils";
 
 import { ReactComponent as Logo } from '../../assets/stack-overflow.svg';
 
@@ -17,58 +18,6 @@ const UserPage = ({ getUser, user: { user, loading }, match  }) => {
         getUser(match.params.id);
         // eslint-disable-next-line
     }, [getUser]);
-
-    const MONTH_NAMES = [
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
-    ];
-    function getFormattedDate(date, prefomattedDate = false, hideYear = false) {
-        const day = date.getDate();
-        const month = MONTH_NAMES[date.getMonth()];
-        const year = date.getFullYear();
-        const hours = date.getHours();
-        let minutes = date.getMinutes();
-        if (minutes < 10) {
-            minutes = `0${ minutes }`;
-        }
-        if (prefomattedDate) {
-            return `${ prefomattedDate } at ${ hours }:${ minutes }`;
-        }
-        if (hideYear) {
-            return `${ day }. ${ month } at ${ hours }:${ minutes }`;
-        }
-        return `${ day }. ${ month } ${ year }. at ${ hours }:${ minutes }`;
-    }
-    function timeAgo(dateParam) {
-        if (!dateParam) {
-            return null;
-        }
-        const date = typeof dateParam === 'object' ? dateParam : new Date(dateParam);
-        const DAY_IN_MS = 86400000; // 24 * 60 * 60 * 1000
-        const today = new Date();
-        const yesterday = new Date(today - DAY_IN_MS);
-        const seconds = Math.round((today - date) / 1000);
-        const minutes = Math.round(seconds / 60);
-        const isToday = today.toDateString() === date.toDateString();
-        const isYesterday = yesterday.toDateString() === date.toDateString();
-        const isThisYear = today.getFullYear() === date.getFullYear();
-        if (seconds < 5) {
-            return 'now';
-        } else if (seconds < 60) {
-            return `${ seconds } seconds ago`;
-        } else if (seconds < 90) {
-            return 'about a minute ago';
-        } else if (minutes < 60) {
-            return `${ minutes } minutes ago`;
-        } else if (isToday) {
-            return getFormattedDate(date, 'Today'); // Today at 10:20
-        } else if (isYesterday) {
-            return getFormattedDate(date, 'Yesterday'); // Yesterday at 10:20
-        } else if (isThisYear) {
-            return getFormattedDate(date, false, true); // 10. January at 10:20
-        }
-        return getFormattedDate(date); // 10. January 2017. at 10:20
-    }
 
     return loading || user === null ? <Fragment>Loading...</Fragment> : <Fragment>
         <div className='page'>

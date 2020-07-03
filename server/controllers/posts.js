6670f4e22652fb088392be2f6925965c3f7d5738
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-07-04 00:57:32 +0530

    feat(#1): create comment and answer model

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -29,37 +29,13 @@ const getPosts = (req, res) => {
 
 const getSinglePost = (req,res) => {
     try {
-        const q = ` SELECT 
-                    posts.id,posts.user_id,tag_id,COUNT(DISTINCT answers.id) 
-                    as answer_count,COUNT(DISTINCT comments.id) 
-                    as comment_count,username,title,posts.body 
-                    as post_body,tagname,posts.created_at 
-                    FROM posts 
-                    JOIN posttag ON posts.id = post_id 
-                    JOIN tags ON tag_id = tags.id 
-                    JOIN users ON user_id = users.id 
-                    LEFT JOIN answers ON answers.post_id = posts.id 
-                    LEFT JOIN comments ON posts.id = comments.post_id 
-                    WHERE posts.id = ?;`;
-        pool.query(q,
-            req.params.id,
-            (err, results) => {
-                if (err) {
-                    console.log(err);
-                    return res
-                        .status(err.statusCode)
-                        .json(helperFunction.responseHandler(false, err.statusCode, err.message, null));
-                }
-                if (results.length === 0){
-                    return res
-                        .status(400)
-                        .json(helperFunction.responseHandler(false, 400, 'There isn\'t any post by this id', null));
-                }
-
-                return res
-                    .status(200)
-                    .json(helperFunction.responseHandler(true, 200, 'success', results[0]));
-            });
+        Post.retrieveOne(req.params.id, (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.status(err.code).json(err);
+            }
+            return res.status(data.code).json(data);
+        });
     } catch (err) {
         console.log(err);
         return res
---
+++ server/models/answers.model.js
@@ -0,0 +1,80 @@
+const helperFunction = require('../helpers/helperFunction');
+
+// constructor
+const Answer = function(answer) {
+    this.text = answer.text;
+    this.user_id = answer.user_id;
+    this.post_id = answer.post_id;
+};
+
+Answer.create = (newAnswer, result) => {
+    const query = `INSERT INTO answers(text,user_id,post_id) VALUES(?,?,?);`;
+
+    pool.query(
+        query,
+        [newAnswer.text, newAnswer.user_id, newAnswer.post_id ],
+        (err,res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(
+                    helperFunction.responseHandler(false, err.statusCode, err.message, null),
+                    null
+                );
+                return;
+            }
+            result(
+                null,
+                helperFunction.responseHandler(true, 200, 'Answer Added', res.insertId)
+            );
+        });
+};
+
+Answer.remove = (id, result) => {
+    const query = ` DELETE FROM answers WHERE id = ?;`;
+
+    pool.query(query,
+        id,
+        (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(
+                    helperFunction.responseHandler(false, err.statusCode, err.message, null),
+                    null
+                );
+                return;
+            }
+            result(
+                null,
+                helperFunction.responseHandler(true, 200, 'Answer Removed', null)
+            );
+        });
+}
+
+Answer.retrieveAll = (postId, result) => {
+    let query = ` SELECT
+                    answers.id, post_id, answers.user_id, username, answers.text, answers.created_at 
+                    FROM answers 
+                    JOIN posts ON posts.id = post_id 
+                    JOIN users ON users.id = answers.user_id 
+                    WHERE post_id = ?;`;
+
+    pool.query(query,
+        postId,
+        (err, results) => {
+            if (err || results.length === 0) {
+                console.log("error: ", err);
+                result(
+                    helperFunction.responseHandler(false, err ? err.statusCode : 404, err ? err.message : 'There are no answers', null),
+                    null
+                );
+                return;
+            }
+            result(
+                null,
+                helperFunction.responseHandler(true, 200, 'Success', results)
+            );
+        });
+}
+
+
+module.exports = Answer;

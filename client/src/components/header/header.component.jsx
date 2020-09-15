e825418ce5a7a6c7d94b375554ca3066711207ef
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-09-15 22:47:02 +0530

    feat: customised web app logo

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../redux/auth/auth.actions'
 
-import { ReactComponent as Logo } from '../../assets/LogoMd.svg';
 import { ReactComponent as Search } from '../../assets/Search.svg';
 
 import './header.styles.scss';

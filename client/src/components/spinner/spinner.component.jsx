3651b31db9e412534ada6afc7f13406ce9499541
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-09-21 00:04:19 +0530

    fix: page spinner fixes

--- client/src/components/spinner/spinner.component.jsx
+++ client/src/components/spinner/spinner.component.jsx
@@ -1,23 +1,13 @@
 import React, {Fragment} from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { ReactComponent as SpinnerGif } from '../../assets/Spinner.svg';
+import { ReactComponent as PageSpinner } from '../../assets/PageSpinner.svg';
 import './spinner.styles.scss';
 
 const Spinner = ({type}) => {
-    const gif = (
-        <SpinnerGif/>
-    )
-
     return (
         <div>
-            { type === 'page' ? <Fragment><SpinnerGif/></Fragment> : 'Loading...' }
+            { type === 'page' ? <Fragment><PageSpinner/></Fragment> : 'Loading...' }
         </div>
     );
 };
 
-// Spinner.propTypes = {
-//     type: PropTypes.object.isRequired
-// };
-
-export default connect(null)(Spinner);
+export default Spinner;

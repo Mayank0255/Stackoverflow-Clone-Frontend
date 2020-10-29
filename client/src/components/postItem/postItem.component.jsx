03f81f4a816feb49f342ce27b89479673f8f88cb
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-10-29 14:01:27 +0530

    feat: implement user gravatars

--- client/src/components/postItem/postItem.component.jsx
+++ client/src/components/postItem/postItem.component.jsx
@@ -55,7 +55,7 @@ const PostItem = ({ post: { id, title, body, tagname, username, user_id, answer_
                             <Link to={`/users/${user_id}`}>
                                 <div className='logo-wrapper'>
                                     <img alt='userlogo'
-                                         src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAQlBMVEXw8PB+3GTy8PJ221n28fd83GGV4ILb7Nea4InX69L08fXe7duP33uW4IP78v1421zs7+tt2k2n4pmD3WvG6L6I3XHpYYlTAAAB70lEQVR4nO3dSU7DQBCG0QzOQGxjhnD/q8IqW6ql6qYi3neAsh8iu+jPbidJkiRJ0n/t8MSFgG+X522OEA+X9+PTtkwh4XH/rJ3OhITVIySsHyFh/QgJ60dIWD9CwvoREtaPkLB+hIT1IySsHyFh/QgfwlOg4CPHntpiwnk5/976GXqrZR166vxxCwh3hynQ6zXypz9exp6aQsBYU+y1XgLfb0k8lRkhYZ9TmRES9jmVGSFhn1OZERL2OZUZIWGfU5kREvY5lRkhYZ9TmRES9jmVGSFhn1OZERL2OZUZIWGfU9Ei6wx/ILyNXY2Y18HC7T4PXo0IfUckUbjfSq5GZAojjf9OFCFha4SE7REStkZI2B4hYWuEhO0RErZGSNgeIWFrhITtERK2RkjYHiFha4QPYWjrIVJwcWDLel7makSodTlfA31F9iBiJa5GxJoj/w7b+pr2wMTViFCxj3Tsw1MzQsL6ERLWj5CwfoSE9SMkrB8hYf0ICetHSFg/QsL6ERLWj5Cw34tldZtDwnVKe2LiakSo+b5FhEtkDyL4xMTViFAR4A8x7XnjVyNG55d0COtHSFg/QsL6ERLWj5CwfoSE9SMkrB8hYf0ICetHSFg/QsL6ET6EWSMOwxu+GjG+4asRwxu9GiFJkiRJUpm+AcyRpXgH9yu9AAAAAElFTkSuQmCC'/>
+                                         src={`https://secure.gravatar.com/avatar/${user_id}?s=164&d=identicon`}/>
                                 </div>
                             </Link>
                         </div>

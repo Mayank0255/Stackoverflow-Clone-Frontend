b4234e49952b76979ec4c1fb1c1486283841e5d2
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-11-03 23:39:09 +0530

    feat(#15): implement search users functionality

--- client/src/pages/UsersPage/UsersPage.component.jsx
+++ client/src/pages/UsersPage/UsersPage.component.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment, useEffect} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getUsers } from '../../redux/users/users.actions';
@@ -8,11 +8,19 @@ import SideBar from '../../components/sideBar/sideBar.component';
 import UserPanel from './UserPanel.component';
 import RightSideBar from '../../components/rightSideBar/rightSideBar.component';
 import Spinner from "../../components/spinner/spinner.component";
+import {ReactComponent as Search} from "../../assets/Search.svg";
 
 const UsersPage = ({ getUsers, user: { users, loading }  }) => {
     useEffect(() => {
         getUsers();
     }, [getUsers]);
+
+    const [ fetchSearch, setSearch] = useState('');
+
+    const handleChange = e => {
+        e.preventDefault();
+        setSearch(e.target.value);
+    };
 
     return loading || users === null ? <Spinner type='page' width='75px' height='200px'/> : <Fragment>
         <div className='page'>
@@ -21,11 +29,23 @@ const UsersPage = ({ getUsers, user: { users, loading }  }) => {
                 <div id='mainbar' className='users-page fc-black-800'>
                     <h1 className='headline'>Users</h1>
                     <div className='headline-count'>
-                        <span>1,025 users</span>
+                        <div>
+                            <span>1,025 users</span>
+                            <form id="search"
+                                  className="grid--cell fl-grow1 searchbar pt12 js-searchbar " autoComplete="off">
+                                <div className="ps-relative search-frame">
+                                    <input className="s-input s-input__search h100 search-box" autoComplete="off"
+                                           type="text" maxLength="35" placeholder="Filter by user"
+                                           onChange= {handleChange}
+                                    />
+                                    <Search/>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                     <div className='user-browser'>
                         <div className='grid-layout'>
-                            {users.map(user => (
+                            {users.filter(user => user.username.toLowerCase().includes(fetchSearch.toLowerCase())).map(user => (
                                 <UserPanel key={user.id} user = {user}/>))}
                         </div>
                     </div>

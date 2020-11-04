39757ee4fc1d681089421ad4b8f39d6969b4aacb
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-11-04 16:01:09 +0530

    feat(#15): implement fetchTagDesc

---
+++ server/constants.js
@@ -0,0 +1,5 @@
+const API_BASE_URL = 'https://api.stackexchange.com/2.2';
+
+module.exports = constantsHolder = {
+    API_BASE_URL
+}
---
+++ server/services/investApi.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const constantsHolder = require('../constants');
+
+const fetchTagDesc = async tag => {
+    const url = constantsHolder.API_BASE_URL + `/tags/${tag}/wikis?site=stackoverflow`;
+
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        json: true
+    };
+
+    return await axios.get(url, options)
+        .then(response => {
+            return response.data.items[0].excerpt;
+        })
+        .catch(err => {
+            console.log('error:', err)
+            return '';
+        })
+}
+
+module.exports = investApi = {
+    fetchTagDesc
+}

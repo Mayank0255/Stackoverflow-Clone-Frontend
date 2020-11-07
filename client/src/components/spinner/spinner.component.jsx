962e08305fbe2149cd151d75062552047818e403
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-11-07 15:55:22 +0530

    refactor(#15): implement AuthForm & restructure alert and header

--- client/src/components/spinner/spinner.component.jsx
+++ client/src/components/spinner/spinner.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './spinner.styles.scss';
 
 import { ReactComponent as PageSpinner } from '../../assets/PageSpinner.svg';
-import { ReactComponent as ComponentSpinner } from '../../assets/ComponentSpinner.svg';
+import { ReactComponent as ComponentSpinner } from '../../assets/three-dots.svg';
 
 const Spinner = ({ type, width, height }) => {
     return (

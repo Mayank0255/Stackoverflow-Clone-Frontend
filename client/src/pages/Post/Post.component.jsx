918564f5f85a7edeb31269bd91f6bb5cbe87b888
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-11-09 18:50:40 +0530

    feat(#15): implement tags redirect state variable

--- client/src/pages/Post/Post.component.jsx
+++ client/src/pages/Post/Post.component.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, Fragment } from 'react';
 import moment from 'moment';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getPost } from '../../redux/posts/posts.actions';
--- client/src/pages/TagPage/TagPage.component.jsx
+++ client/src/pages/TagPage/TagPage.component.jsx
@@ -1,40 +1,42 @@
 import React, {useEffect, Fragment, useState} from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { getTagPosts } from '../../redux/posts/posts.actions';
-import handleSorting from "../../services/handleSorting";
+import { getTag } from '../../redux/tags/tags.actions';
+import handleSorting from '../../services/handleSorting';
 
-import LinkButton from "../../components/LinkButton/LinkButton.component";
+import LinkButton from '../../components/LinkButton/LinkButton.component';
 import SideBar from '../../components/sideBar/sideBar.component';
 import PostItem from '../../components/postItem/postItem.component';
 import RightSideBar from '../../components/rightSideBar/rightSideBar.component';
-import Spinner from "../../components/spinner/spinner.component";
-import PageTitle from "../../components/pageTitle/pageTitle.component";
-import ButtonGroup from "../../components/ButtonGroup/ButtonGroup.component";
+import Spinner from '../../components/spinner/spinner.component';
+import PageTitle from '../../components/pageTitle/pageTitle.component';
+import ButtonGroup from '../../components/ButtonGroup/ButtonGroup.component';
 
 import './TagPage.styles.scss';
 
-const TagPage = ({ getTagPosts, post: { posts, loading }, match  }) => {
+const TagPage = ({ getTag, getTagPosts, tag, post: { posts, loading }, match  }) => {
     useEffect(() => {
         getTagPosts(match.params.tagname);
+        getTag(match.params.tagname)
         // eslint-disable-next-line
-    }, [getTagPosts]);
+    }, [getTag, getTagPosts]);
 
     const [sortType, setSortType] = useState('Newest');
 
-    if (posts.length === 0) {
-        return <Redirect to='/tags'/>;
+    if (tag.redirect) {
+        return <Redirect to='/tags' />;
     }
 
-    return loading ? <Spinner type='page' width='75px' height='200px'/> : <Fragment>
-        <PageTitle title={`Questions tagged [${match.params.tagname}] - CLONE Stack Overflow`}/>
+    return tag.tag === null || tag.loading || loading ? <Spinner type='page' width='75px' height='200px'/> : <Fragment>
+        <PageTitle title={`Questions tagged [${tag.tag.tagname}] - CLONE Stack Overflow`}/>
         <div className='page'>
             <SideBar/>
             <div id="content">
                 <div id='mainbar' className='questions-page fc-black-800'>
                     <div className='questions-grid'>
-                        <h3 className='questions-headline'>Questions tagged [{match.params.tagname}]</h3>
+                        <h3 className='questions-headline'>Questions tagged [{tag.tag.tagname}]</h3>
                         <div className='questions-btn'>
                             <LinkButton
                                 text={'Ask Question'}
@@ -45,10 +47,10 @@ const TagPage = ({ getTagPosts, post: { posts, loading }, match  }) => {
                     </div>
                     <p
                         className='fs-body'
-                        dangerouslySetInnerHTML={{ __html: posts.length === 0 ? '' : posts[0].description}}
+                        dangerouslySetInnerHTML={{ __html: tag.tag.description }}
                     />
                     <div className='questions-tabs'>
-                        <span>19,204,360 questions</span>
+                        <span>{tag.tag.posts_count} {tag.tag.posts_count === 1 ? 'question' : 'questions'}</span>
                         <ButtonGroup
                             buttons={['Newest', 'Top', 'Views', 'Oldest']}
                             selected={sortType}
@@ -56,7 +58,7 @@ const TagPage = ({ getTagPosts, post: { posts, loading }, match  }) => {
                         />
                     </div>
                     <div className='questions'>
-                        {posts.length === 0 ? ( <h4 style={{margin: '30px 30px'}}>There are no questions from this tag</h4> ) :
+                        {tag.tag.posts_count === 0 ? ( <h4 style={{margin: '30px 30px'}}>There are no questions from this tag</h4> ) :
                             posts
                                 ?.sort(handleSorting(sortType))
                                 .map(post => (
@@ -73,12 +75,15 @@ const TagPage = ({ getTagPosts, post: { posts, loading }, match  }) => {
 
 
 TagPage.propTypes = {
+    getTag: PropTypes.func.isRequired,
     getTagPosts: PropTypes.func.isRequired,
-    post: PropTypes.object.isRequired
+    post: PropTypes.object.isRequired,
+    tag: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
-    post: state.post
+    post: state.post,
+    tag: state.tag
 });
 
-export default connect(mapStateToProps, { getTagPosts })(TagPage);
+export default connect(mapStateToProps, { getTagPosts, getTag })(TagPage);

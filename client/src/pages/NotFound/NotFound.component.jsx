f470e288eaf46ca52e265c696ab36f9a3db83da8
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-11-29 23:52:10 +0530

    feat(#20): implement error 404 page component

---
+++ client/src/pages/NotFound/NotFound.component.jsx
@@ -0,0 +1,47 @@
+import React, { Fragment } from 'react';
+import {Link} from "react-router-dom";
+
+import './NotFound.styles.scss';
+
+const NotFound = () => {
+    return <Fragment>
+        <div className="page">
+            <div className="box">
+                <div className="box__ghost">
+                    <div className="symbol"></div>
+                    <div className="symbol"></div>
+                    <div className="symbol"></div>
+                    <div className="symbol"></div>
+                    <div className="symbol"></div>
+                    <div className="symbol"></div>
+
+                    <div className="box__ghost-container">
+                        <div className="box__ghost-eyes">
+                            <div className="box__eye-left"></div>
+                            <div className="box__eye-right"></div>
+                        </div>
+                        <div className="box__ghost-bottom">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className="box__ghost-shadow"></div>
+                </div>
+                <div className="box__description">
+                    <div className="box__description-container">
+                        <div className="box__description-title fc-black-800">Whoops!</div>
+                        <div className="box__description-text fc-black-500">
+                            It seems like we couldn't find the page you were looking for
+                        </div>
+                    </div>
+                    <Link to='/' className='box__button'>Back to home page</Link>
+                </div>
+            </div>
+        </div>
+    </Fragment>
+}
+
+export default NotFound;

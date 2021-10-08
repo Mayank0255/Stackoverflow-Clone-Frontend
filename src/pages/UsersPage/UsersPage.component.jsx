89ce654a48ee175346f9c10499693704495246e4
Sai Sridhar Akula <sridhar.akula001@gmail.com>
2021-10-08 19:11:46 +0530

    Minor changes

--- src/pages/UsersPage/UsersPage.component.jsx
+++ src/pages/UsersPage/UsersPage.component.jsx
@@ -11,7 +11,7 @@ import ButtonGroup from '../../components/ButtonGroup/ButtonGroup.component';
 
 import './UsersPage.styles.scss';
 
-const UsersPage = ({getUsers, user: {users0, loading}}) => {
+const UsersPage = ({getUsers, user: {users, loading}}) => {
   useEffect(() => {
     getUsers();
   }, [getUsers]);
@@ -24,7 +24,7 @@ const UsersPage = ({getUsers, user: {users0, loading}}) => {
     setSearch(e.target.value);
   };
 
-  let users = ["Bankai","Chika","Pucci","Kenpachi"].map(user => ({username: user}))
+  // let users = ["Bankai","Chika","Pucci","Kenpachi"].map(user => ({username: user}))
 
   console.log(users);
 

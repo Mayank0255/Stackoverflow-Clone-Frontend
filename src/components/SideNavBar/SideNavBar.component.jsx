62bd879c851de830bda9aa47a6db07d46b17ca2d
Sai Sridhar Akula <sridhar.akula001@gmail.com>
2021-10-09 20:59:36 +0530

    Side Menu

--- src/components/SideNavBar/SideNavBar.component.jsx
+++ src/components/SideNavBar/SideNavBar.component.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import {Link} from 'react-router-dom';
 
 import { ReactComponent as Hamburger } from "../../assets/Hamburger.svg";
+import { ReactComponent as Stack } from "../../assets/LogoMd.svg";
 import "./SideNavBar.styles.scss";
 
 const SidebarUI = ({ isOpen, ...rest }) => {
@@ -35,11 +37,32 @@ const SideNavBar = (props) => {
 
 	const { hasOverlay, isRight } = props;
 
+	const guestTabs = (
+		<div className='side-s-navigation'>
+		  <Link to='/' className='side-s-navigation--item is-selected'>
+			Products
+		  </Link>
+		  <Link to='/' className='side-s-navigation--item not-selected'>
+			Customers
+		  </Link>
+		  <Link to='/' className='side-s-navigation--item not-selected'>
+			Use cases
+		  </Link>
+		</div>
+	  );
+
 	return (
 		<SidebarUI isOpen={isOpen}>
 			<Hamburger onClick={openSidebar} className="ham" />
 
-			<SidebarUI.Content isRight={isRight}>hi</SidebarUI.Content>
+			<SidebarUI.Content isRight={isRight} onClick={() => openSidebar(false)}>
+				<div className="content-logo">
+					<Stack />
+				</div>
+				<div className="content-inner">
+					{guestTabs}
+				</div>
+			</SidebarUI.Content>
 			{hasOverlay ? (
 				<SidebarUI.Overlay onClick={() => openSidebar(false)} />
 			) : (

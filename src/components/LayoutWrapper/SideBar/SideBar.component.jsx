8ceaad9576fd894fd22c4bbfe7fec54e452344e0
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2022-03-18 00:03:43 +0530

    refactor: improve sidebar code structure

--- src/components/LayoutWrapper/SideBar/SideBar.component.jsx
+++ src/components/LayoutWrapper/SideBar/SideBar.component.jsx
@@ -1,35 +1,25 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-
-import { ListItem, ListItemButton, ListItemText } from '@mui/material';
 
 import SideBarItem from "./SideBarItem.component";
-import { ReactComponent as GlobalIcon } from '../../../assets/Globe.svg';
+import { SideBarData } from "./SideBarData";
 
 import './SideBar.styles.scss';
 
 const SideBar = () => (
   <div className='side-bar-container'>
     <div className='side-bar-tabs'>
-      <NavLink
-        exact
-        activeClassName='active'
-        className='home-link'
-        to='/'
-      >
-        <ListItem disablePadding>
-          <ListItemButton style={{ paddingLeft: '8px' }}>
-            <ListItemText className='menu-list-text' primary="Home" />
-          </ListItemButton>
-        </ListItem>
-      </NavLink>
+      <SideBarItem isHome={true} link='/' text='Home'/>
 
       <div className='public-tabs'>
         <p className='title fc-light'>PUBLIC</p>
-        <SideBarItem link='/questions' icon={<GlobalIcon className='icon' />} text='Stack Overflow'/>
-        <SideBarItem link='/tags' text='Tags'/>
-        <SideBarItem link='/users' text='Users'/>
-        <SideBarItem link='/jobs' text='Jobs'/>
+        {SideBarData.map(({ link, icon, text}, index) => (
+          <SideBarItem
+            key={index}
+            link={link}
+            icon={icon}
+            text={text}
+          />
+        ))}
       </div>
       <div className='teams-tabs'>
         <p className='title fc-light'>TEAMS</p>
--- src/components/LayoutWrapper/SideBar/SideBarItem.component.jsx
+++ src/components/LayoutWrapper/SideBar/SideBarItem.component.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 
 import './SideBar.styles.scss';
 
-const SideBarItem = ({ link, icon, text }) => (
+const SideBarItem = ({ link, icon, text, isHome }) => {
+  return (
+    <Fragment>
+      {isHome ?
+        <HomeItem
+          link={link}
+          text={text}
+        /> :
+        <DefaultItem
+          link={link}
+          icon={icon}
+          text={text}
+        />
+      }
+    </Fragment>
+  )
+};
+
+const HomeItem = ({ link, text }) => (
+  <NavLink
+    exact
+    activeClassName='active'
+    className='home-link'
+    to={link}
+  >
+    <ListItem disablePadding>
+      <ListItemButton style={{ paddingLeft: '8px' }}>
+        <ListItemText className='menu-list-text' primary={text} />
+      </ListItemButton>
+    </ListItem>
+  </NavLink>
+)
+
+const DefaultItem = ({ link, icon, text }) => (
   <NavLink
     activeClassName='active'
     className='icon-link'
@@ -16,10 +49,14 @@ const SideBarItem = ({ link, icon, text }) => (
         <ListItemIcon className='menu-list-icon'>
           {icon}
         </ListItemIcon>
-        <ListItemText className='menu-list-text' primary={text} />
+        <ListItemText className='menu-list-text' primary={text}/>
       </ListItemButton>
     </ListItem>
   </NavLink>
-);
+)
+
+SideBarItem.defaultProps = {
+  isHome: false,
+};
 
 export default SideBarItem;

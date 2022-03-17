2e1a1caaabdeec688eebea88233b0f09b69d9ec6
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2022-03-17 13:25:18 +0530

    feat: add mui pagination

--- src/pages/AllTagsPage/AllTagsPage.component.jsx
+++ src/pages/AllTagsPage/AllTagsPage.component.jsx
@@ -4,11 +4,13 @@ import PropTypes from 'prop-types';
 import {getTags} from '../../redux/tags/tags.actions';
 import handleSorting from '../../services/handleSorting';
 
+import { Pagination, Stack } from '@mui/material';
+
 import TagPanel from './TagPanel/TagPanel.component';
 import Spinner from '../../components/Spinner/Spinner.component';
 import SearchBox from '../../components/SearchBox/SearchBox.component';
 import ButtonGroup from '../../components/ButtonGroup/ButtonGroup.component';
-import Pagination from '../../components/Pagination/Pagination.component';
+import PaginationOne from '../../components/Pagination/Pagination.component';
 
 import './AllTagsPage.styles.scss';
 
@@ -28,6 +30,8 @@ const AllTagsPage = ({getTags, tag: {tags, loading}}) => {
     e.preventDefault();
     setSearch(e.target.value);
   };
+
+  const handleChange1 = (e, value) => setPage(value);
 
   const handlePaginationChange = (currentPage) => setPage(currentPage);
 
@@ -71,6 +75,13 @@ const AllTagsPage = ({getTags, tag: {tags, loading}}) => {
           </div>
         </div>
         <Pagination
+          count={Math.ceil(tags.filter((tag) => tag.tagname.toLowerCase().includes(fetchSearch.toLowerCase())).length/itemsPerPage)}
+          page={page}
+          variant="outlined"
+          shape="rounded"
+          onChange={handleChange1}
+        />
+        <PaginationOne
           total={tags.filter((tag) => tag.tagname.toLowerCase().includes(fetchSearch.toLowerCase())).length}
           elementsPerPage={itemsPerPage}
           showInline={showInline}

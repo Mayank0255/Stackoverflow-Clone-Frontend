e90af8e8552c3650530d7cafc5b15291c1fc4999
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2022-03-18 23:17:08 +0530

    refactor: trim SideBarWidget component

--- src/components/LayoutWrapper/RightSideBar/SideBarWidget/SideBarWidget.component.jsx
+++ src/components/LayoutWrapper/RightSideBar/SideBarWidget/SideBarWidget.component.jsx
@@ -1,160 +1,55 @@
 import React, {Fragment} from 'react';
 import {Link} from 'react-router-dom';
 
-import {ReactComponent as EditLogo} from '../../../../assets/Edit.svg';
+import { SideBarWidgetData } from "./SideBarWidgetData";
 
 import './SideBarWidget.styles.scss';
 
 const SideBarWidget = () => {
   return (
     <Fragment>
-      <div
-        className='s-sidebarwidget s-sidebarwidget__yellow s-anchors s-anchors__grayscale mb16'
-        data-tracker='cb=1'
-      >
-        <ul className='d-block p0 m0'>
-          <div className='s-sidebarwidget--header s-sidebarwidget__small-bold-text d:fc-black-900 bb bbw1'>
-            The Overflow Blog
-          </div>
-          <li className='s-sidebarwidget--item grid px16'>
-            <div className='grid--cell1 fl-shrink0'>
-              <EditLogo style={{color: '#f2f2f3'}}/>
-            </div>
-            <div className='grid--cell'>
-              <Link
-                to='https://stackoverflow.blog/2020/07/13/tales-from-documentation-write-for-your-dumbest-user/'
-                className='js-gps-track'
-                data-ga='["community bulletin board","The Overflow Blog","https://stackoverflow.blog/2020/07/13/tales-from-documentation-write-for-your-dumbest-user/",null,null]'
-                data-gps-track='communitybulletin.click({ priority: 1, position: 0 })'
-              >
-                Tales from documentation: Write for your clueless users
-              </Link>
-            </div>
-          </li>
-          <li className='s-sidebarwidget--item grid px16'>
-            <div className='grid--cell1 fl-shrink0'>
-              <EditLogo style={{color: '#f2f2f3'}}/>
-            </div>
-            <div className='grid--cell'>
-              <Link
-                to='https://stackoverflow.blog/2020/07/14/podcast-252-a-conversation-on-diversity-and-representation/'
-                className='js-gps-track'
-                data-ga='["community bulletin board","The Overflow Blog","https://stackoverflow.blog/2020/07/14/podcast-252-a-conversation-on-diversity-and-representation/",null,null]'
-                data-gps-track='communitybulletin.click({ priority: 1, position: 1 })'
-              >
-                Podcast 252: a conversation on diversity and representation
-              </Link>
-            </div>
-          </li>
-          <div className='s-sidebarwidget--header s-sidebarwidget__small-bold-text d:fc-black-900 bb bbw1'>
-            Upcoming Events
-          </div>
-          <li className='s-sidebarwidget--item grid px16'>
-            <div className='grid--cell1 fl-shrink0'>
-              <div
-                className='favicon favicon-stackoverflow'
-                title='Stack Overflow'
+      <div className="s-sidebarwidget s-sidebarwidget__yellow s-anchors s-anchors__grayscale mb16" data-tracker="cb=1">
+        <ul className="d-block p0 m0">
+          {SideBarWidgetData.map(({ type, title, icon, link }, index) => {
+            if (type === 'header') {
+              return <WidgetHeader
+                key={index}
+                title={title}
               />
-            </div>
-            <div className='grid--cell'>
-              <Link
-                to='https://stackoverflow.com/election'
-                className='fc-danger js-gps-track'
-                data-ga='["community bulletin board","Upcoming Events","https://stackoverflow.com/election",null,null]'
-                data-gps-track='communitybulletin.click({ priority: 5, position: 2 })'
-              >
-                2020 Community Moderator Election
-              </Link>
-              <div style={{color: '#f2f2f3'}}>
-                ends{' '}
-                <span title='2020-07-21 20:00:00Z' className='relativetime'>
-                  in 6 days
-                </span>
-              </div>
-            </div>
-          </li>
-          <div className='s-sidebarwidget--header s-sidebarwidget__small-bold-text d:fc-black-900 bb bbw1'>
-            Featured on Meta
-          </div>
-          <li className='s-sidebarwidget--item grid px16'>
-            <div className='grid--cell1 fl-shrink0'>
-              <div
-                className='favicon favicon-stackexchangemeta'
-                title='Meta Stack Exchange'
+            } else {
+              return <WidgetItem
+                key={index}
+                icon={icon}
+                title={title}
+                link={link}
               />
-            </div>
-            <div className='grid--cell'>
-              <Link
-                to='https://meta.stackexchange.com/questions/350184/feedback-post-new-moderator-reinstatement-and-appeal-process-revisions'
-                className='js-gps-track'
-                data-ga='["community bulletin board","Featured on Meta","https://meta.stackexchange.com/questions/350184/feedback-post-new-moderator-reinstatement-and-appeal-process-revisions",null,null]'
-                data-gps-track='communitybulletin.click({ priority: 3, position: 3 })'
-              >
-                Feedback post: New moderator reinstatement and appeal process
-                revisions
-              </Link>
-            </div>
-          </li>
-          <li className='s-sidebarwidget--item grid px16'>
-            <div className='grid--cell1 fl-shrink0'>
-              <div
-                className='favicon favicon-stackexchangemeta'
-                title='Meta Stack Exchange'
-              />
-            </div>
-            <div className='grid--cell'>
-              <Link
-                to='https://meta.stackexchange.com/questions/350544/the-new-moderator-agreement-is-now-live-for-moderators-to-accept-across-the-netw'
-                className='js-gps-track'
-                title='The new moderator agreement is now live for moderators to accept across the network'
-                data-ga='["community bulletin board","Featured on Meta","https://meta.stackexchange.com/questions/350544/the-new-moderator-agreement-is-now-live-for-moderators-to-accept-across-the-netw",null,null]'
-                data-gps-track='communitybulletin.click({ priority: 3, position: 4 })'
-              >
-                The new moderator agreement is now live for moderators to accept
-                across the&hellip;
-              </Link>
-            </div>
-          </li>
-          <li className='s-sidebarwidget--item grid px16'>
-            <div className='grid--cell1 fl-shrink0'>
-              <div
-                className='favicon favicon-stackoverflowmeta'
-                title='Meta Stack Overflow'
-              />
-            </div>
-            <div className='grid--cell'>
-              <Link
-                to='https://meta.stackoverflow.com/questions/399013/allow-bountied-questions-to-be-closed-by-regular-users'
-                className='js-gps-track'
-                data-ga='["community bulletin board","Featured on Meta","https://meta.stackoverflow.com/questions/399013/allow-bountied-questions-to-be-closed-by-regular-users",null,null]'
-                data-gps-track='communitybulletin.click({ priority: 6, position: 5 })'
-              >
-                Allow bountied questions to be closed by regular users
-              </Link>
-            </div>
-          </li>
-          <li className='s-sidebarwidget--item grid px16'>
-            <div className='grid--cell1 fl-shrink0'>
-              <div
-                className='favicon favicon-stackoverflowmeta'
-                title='Meta Stack Overflow'
-              />
-            </div>
-            <div className='grid--cell'>
-              <Link
-                to='https://meta.stackoverflow.com/questions/399106/2020-community-moderator-election-questionnaire'
-                className='js-gps-track'
-                data-ga='["community bulletin board","Featured on Meta","https://meta.stackoverflow.com/questions/399106/2020-community-moderator-election-questionnaire",null,null]'
-                data-gps-track='communitybulletin.click({ priority: 6, position: 6 })'
-              >
-                2020 Community Moderator Election - Questionnaire
-              </Link>
-            </div>
-          </li>
+            }
+          })}
         </ul>
       </div>
     </Fragment>
   );
 };
+
+const WidgetHeader = ({ title }) => (
+  <div className="s-sidebarwidget--header s-sidebarwidget__small-bold-text fc-light d:fc-black-900 bb bbw1">
+    {title}
+  </div>
+)
+
+const WidgetItem = ({ icon, title, link }) => (
+  <li className="s-sidebarwidget--item d-flex px16">
+    <div className="flex--item1 fl-shrink0">
+      {icon}
+    </div>
+    <div className="flex--item wmn0 ow-break-word">
+      <Link
+        to={link}
+        className="js-gps-track"
+        data-ga={`[&quot;community bulletin board&quot;,&quot;The Overflow Blog&quot;,&quot;${link}&quot;,null,null]`}
+        data-gps-track="communitybulletin.click({ priority: 1, position: 0 })">{title}</Link>
+    </div>
+  </li>
+)
 
 export default SideBarWidget;
---
+++ src/components/LayoutWrapper/RightSideBar/SideBarWidget/SideBarWidgetData.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+import {ReactComponent as EditLogo} from "../../../../assets/Edit.svg";
+
+export const SideBarWidgetData = [
+  {
+    type: 'header',
+    title: 'The Overflow Blog'
+  },
+  {
+    type: 'item',
+    title: 'Celebrating the Stack Exchange sites that turned ten years old in Q1 2022',
+    icon: <EditLogo/>,
+    link: 'https://stackoverflow.blog/2022/03/16/celebrating-the-stack-exchange-site-that-turned-ten-years-old-in-q1-2022'
+  },
+  {
+    type: 'item',
+    title: 'New data: What makes developers happy at work',
+    icon: <EditLogo/>,
+    link: 'https://stackoverflow.blog/2022/03/17/new-data-what-makes-developers-happy-at-work'
+  },
+  {
+    type: 'header',
+    title: 'Featured on Meta'
+  },
+  {
+    type: 'item',
+    title: 'What goes into site sponsorships on SE?',
+    icon: <div className="favicon favicon-stackexchangemeta" title="Meta Stack Exchange"/>,
+    link: 'https://meta.stackexchange.com/questions/376530/what-goes-into-site-sponsorships-on-se'
+  },
+  {
+    type: 'item',
+    title: 'Stack Exchange Q&amp;A access will not be restricted in Russia',
+    icon: <div className="favicon favicon-stackexchangemeta" title="Meta Stack Exchange"/>,
+    link: 'https://meta.stackexchange.com/questions/376743/stack-exchange-qa-access-will-not-be-restricted-in-russia'
+  },
+  {
+    type: 'item',
+    title: 'Announcing an A/B test for a Trending sort option',
+    icon: <div className="favicon favicon-stackoverflowmeta" title="Meta Stack Overflow"/>,
+    link: 'https://meta.stackoverflow.com/questions/416486/announcing-an-a-b-test-for-a-trending-sort-option'
+  },
+  {
+    type: 'item',
+    title: 'New User Experience: Deep Dive into our Research on the Staging Ground – How...',
+    icon: <div className="favicon favicon-stackoverflowmeta" title="Meta Stack Overflow"/>,
+    link: 'https://meta.stackoverflow.com/questions/416652/new-user-experience-deep-dive-into-our-research-on-the-staging-ground-how-do'
+  },
+  {
+    type: 'header',
+    title: 'Hot Meta Posts'
+  },
+  {
+    type: 'item',
+    title: 'Changing initializer-list tag wiki',
+    icon: <span className="fc-black-500" title="Question score (upvotes - downvotes)">16</span>,
+    link: 'https://meta.stackoverflow.com/questions/416623/changing-initializer-list-tag-wiki'
+  },
+  {
+    type: 'item',
+    title: 'What is the true intention in the "How to reference material written by...',
+    icon: <span className="fc-black-500" title="Question score (upvotes - downvotes)">10</span>,
+    link: 'https://meta.stackoverflow.com/questions/416665/what-is-the-true-intention-in-the-how-to-reference-material-written-by-others'
+  },
+]

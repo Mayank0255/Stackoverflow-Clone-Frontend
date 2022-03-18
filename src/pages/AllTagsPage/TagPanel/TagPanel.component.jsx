a372ff3b67cbd704d29a6b6ef3b509013f8aafaf
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2022-03-18 23:40:44 +0530

    feat: tags fix

--- src/pages/AllTagsPage/TagPanel/TagPanel.component.jsx
+++ src/pages/AllTagsPage/TagPanel/TagPanel.component.jsx
@@ -4,20 +4,20 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import TagBadge from '../../../components/TagBadge/TagBadge.component';
 
-import './TagPanel.styles.scss';
-
 const TagPanel = ({tag: {tagname, description, created_at, posts_count}}) => {
   return (
-    <div className='tag-card'>
-      <div className='grid'>
+    <div className="grid--item s-card js-tag-cell d-flex fd-column">
+      <div className="d-flex jc-space-between ai-center mb12">
         <TagBadge tag_name={tagname} size={'s-tag'} float={'left'} />
       </div>
-      <div className='description'>{description}</div>
-      <div className='tag-caption'>
-        <div className='tag-cell'>
-          {posts_count} {posts_count === 1 ? 'question' : 'questions'}
-        </div>
-        <div className='tag-cell'>
+
+      <div className="flex--item fc-medium mb12 v-truncate4">
+        {description}
+      </div>
+
+      <div className="mt-auto d-flex jc-space-between fs-caption fc-black-400">
+        <div className="flex--item">{posts_count} {posts_count === 1 ? 'question' : 'questions'}</div>
+        <div className="flex--item s-anchors s-anchors__inherit">
           added {moment(created_at).fromNow(false)}
         </div>
       </div>

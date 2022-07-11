144f441c026e64f76612ffca6272d8159a47d355
BurhanuddinRaja <mails4burhan@gmail.com>
2022-07-11 10:36:27 +0530

    Conflicts resolved and changes added

--- src/components/TagBadge/TagBadge.component.jsx
+++ src/components/TagBadge/TagBadge.component.jsx
@@ -3,17 +3,10 @@ import { Link } from 'react-router-dom';
 
 import './TagBadge.styles.scss';
 
-<<<<<<< HEAD
-const TagBadge = ({tag_name, size, display, link, href}) => {
-  return (
-    <Fragment>
-      <div className='tags-badge' style={{ display }}>
-=======
 const TagBadge = ({ tag_name, size, display, link, href }) => {
   return (
     <Fragment>
         <div className='tags-badge' style={{ display }}>
->>>>>>> origin/burhanraja
           {href === true ? (
             <Link className={`${size}`} to={link ? link : `/tags/${tag_name}`}>
               {tag_name}
--- src/components/UserCard/UserCard.component.jsx
+++ src/components/UserCard/UserCard.component.jsx
@@ -16,11 +16,7 @@ const UserCard = ({
     <Fragment>
       <div
         className='owner'
-<<<<<<< HEAD
-        style={{ float: float, backgroundColor: backgroundColor }}
-=======
-        style={{backgroundColor: backgroundColor}}
->>>>>>> origin/burhanraja
+        style={{backgroundColor: backgroundColor }}
       >
         <div className='user-block fc-black-500'>
           <div className='user-logo'>
@@ -34,7 +30,6 @@ const UserCard = ({
             </Link>
           </div>
           <div className="profile-text">
-<<<<<<< HEAD
             <div className='user-profile'>
               <Link
                 className='user-profile-link fc-blue-600'
@@ -47,20 +42,6 @@ const UserCard = ({
               {dateType ? dateType : 'asked'} {moment(created_at).fromNow(true)}{' '}
               ago
             </div>
-=======
-          <div className='user-profile'>
-            <Link
-              className='user-profile-link fc-blue-600'
-              to={`/users/${user_id}`}
-            >
-              {username}
-            </Link>
->>>>>>> origin/burhanraja
-          </div>
-          <div className='action-time'>
-            {dateType ? dateType : 'asked'} {moment(created_at).fromNow(true)}{' '}
-            ago
-          </div>
           </div>
         </div>
       </div>
